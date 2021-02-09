e1d561fc82553317c40a26a98f07fb5a7294081b
Linye1118 <75881110+Linye1118@users.noreply.github.com>
2021-02-10 12:52:21 +1300

    Adjust 1page search bar

--- WorkSafeWebSite/wwwroot/js/site.js
+++ WorkSafeWebSite/wwwroot/js/site.js
@@ -21,21 +21,6 @@ function toggle_topic_visibility(id) {
     }
 }
 
-/*try new search bar*/
-$(document).ready(function () {
-    $(".default_option").click(function () {
-        $(".dropdown ul").addClass("active");
-    });
-
-    $(".dropdown ul li").click(function () {
-        var text = $(this).text();
-        $(".default_option").text(text);
-        $(".dropdown ul").removeClass("active");
-    });
-});
-
-
-
 /* select topic under industry on page1 */
 
 function selectTopic(e) {
