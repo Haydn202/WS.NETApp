82c899ded677cb9bc088963afbd24ebbee771d8a
Linye1118 <75881110+Linye1118@users.noreply.github.com>
2021-02-12 10:31:01 +1300

    adjust slideshow

--- WorkSafeWebSite/Startup.cs
+++ WorkSafeWebSite/Startup.cs
@@ -63,7 +63,7 @@ namespace WorkSafeWebSite
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Search}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
